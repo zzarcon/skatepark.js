cf98eb4f28bcd18d8570bddfff77398ce6dace0d
Hector Zarco <hezarco@gmail.com>
2016-12-16 02:35:01 +0100

    add minimum behaviour

--- packages/tags/src/index.js
+++ packages/tags/src/index.js
@@ -1,3 +1,70 @@
-import 'skatejs-web-components';
-import { define, vdom } from 'skatejs';
+import {
+  Component,
+  h
+} from 'skatejs';
+import styles from './styles';
 
+const deleteCode = 8;
+
+customElements.define('sk-tags', class extends Component {
+  static get props() {
+    return {
+      delimiter: {
+        attribute: true,
+        default: ' '
+      },
+      tags: {
+        default: []
+      }
+    };
+  }
+
+  renderCallback() {
+    const tags = this.tags;
+    const tagElements = tags.map(t => h('span', {class: 'tag'}, t));
+
+    return [
+      h('style', styles),
+      h('div', {class: 'wrapper'}, 
+        h('span', ...tagElements),
+        h('input', {
+          onkeyup: this.onKey(),
+          autofocus: true,
+          class: 'input'
+        })
+      )
+    ];
+  }
+
+  onKey() {
+    const component = this;
+
+    return function(e) {
+      const lastChar = this.value.substr(-1);
+      const value = this.value.trim();
+      const code = e.keyCode;
+      const isDel = code === deleteCode;
+      const isDelimiter = lastChar === component.delimiter;
+
+      if (isDel && value.length <= 0) {
+        component.removeTag();
+      }
+
+      if (value && isDelimiter) {
+        component.addTag(value);
+        this.value = '';
+      }
+    };
+  }
+
+  addTag(value) {
+    this.tags = this.tags.concat(value);
+  }
+
+  removeTag() {
+    const tags = this.tags.slice();
+
+    tags.pop();
+    this.tags = tags;
+  }
+});
---
+++ packages/tags/src/styles.js
@@ -0,0 +1,19 @@
+export default `
+  .wrapper{
+    border: 1px solid #ccc;
+    width: 600px;
+    padding: 5px;
+    min-height: 38px;
+    display: flex;
+  }
+  .tag{
+    border: 1px solid #45B39E;
+    padding: 3px;
+    margin-right: 10px;
+    display: inline-block;
+  }
+  .input{
+    outline: none;
+    border-color: transparent;
+  }
+`;
